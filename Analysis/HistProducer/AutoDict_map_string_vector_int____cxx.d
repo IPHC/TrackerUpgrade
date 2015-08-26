304eda676a098ef56ce2eaa9697648648199092c
kskovpen <Kirill.Skovpen@cern.ch>
2015-08-26 13:20:56 +0200

    removed

--- Analysis/HistProducer/AutoDict_map_string_vector_int____cxx.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-AutoDict_map_string_vector_int____cxx.so: /cvmfs/cms.cern.ch/slc6_amd64_gcc472/lcg/root/5.34.07-cms3/include/cintdictversion.h /cvmfs/cms.cern.ch/slc6_amd64_gcc472/lcg/root/5.34.07-cms3/include/RVersion.h
-AutoDict_map_string_vector_int____cxx__ROOTBUILDVERSION= 5.34/07
--- Analysis/HistProducer/test/atlasstyle-00-03-04/lib.C
+++
@@ -1,5 +0,0 @@
-{
-   gSystem->CompileMacro("AtlasUtils.C","k","libAtlasUtils");
-   gSystem->CompileMacro("AtlasStyle.C","k","libAtlasStyle");
-   gApplication->Terminate();
-}
